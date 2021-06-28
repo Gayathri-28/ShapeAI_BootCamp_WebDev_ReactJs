4521dfeb6d1f0e532d6d8c1fe222f4e368649957
Gayathri Kolluri <56117754+Gayathri-28@users.noreply.github.com>
2021-06-28 10:16:33 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./Header";
+
+function Footer() {
+  return (
+    <footer>
+      <p> Copyright by ShapeAI @ {new Date().getFullYear()} </p>
+    </footer>
+  );
+}
+
+export default Footer;
